915168facf77fd2d11812847daf4885ae893b7ea
surendrailla36 <surendranadhilla@gmail.com>
2024-09-12 16:06:47 -0500

    I have added dependacy in the pom.xml,I have added entitys to the signup class, I have added source file, I have added applicationproperties.

--- src/main/java/com/example/pocservice/modelclass/signup.java
+++ src/main/java/com/example/pocservice/modelclass/signup.java
@@ -1,9 +1,16 @@
 package com.example.pocservice.modelclass;
 
-public class signup {
-    public static void main (String[] args) {
-        System.out.println("Hello World");
 
-    }
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="signup")
+public class signup {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
+    private int id;
+
 }
+
 
---
+++ src/main/java/com/example/pocservice/pocservice/pocserviceapplication.java
@@ -0,0 +1,11 @@
+package com.example.pocservice.pocservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pocserviceapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(pocserviceapplication.class, args);
+    }
+}
